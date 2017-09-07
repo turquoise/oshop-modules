ceb5655717e32159f7342e6ce32ac6dead26ed39
M Gosschalk <monica.gosschalk@btinternet.com>
2017-09-07 21:30:42 +0100

    added sticky top categories

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from '../models/product';
+import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'app-products',
@@ -7,7 +11,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsComponent implements OnInit {
 
-  constructor() { }
+  products: Product[] = [];
+  filteredProducts: Product[] = [];
+
+  category: string = '';
+
+  constructor(
+    private productService: ProductService,
+    private route: ActivatedRoute ) {
+
+    // productService.getAll()
+    //   .subscribe( products => {
+    //     this.products = products
+    //
+    //     route.queryParamMap.subscribe( params => {
+    //       this.category = params.get('category');
+    //
+    //       this.filteredProducts = (this.category) ?
+    //         this.products.filter( p => p.category === this.category) :
+    //         this.products;
+    //     });
+    //   });
+
+      productService
+        .getAll()
+        .switchMap( products => {
+          this.products = products
+          return route.queryParamMap;
+        })
+        .subscribe( params => {
+          this.category = params.get('category');
+
+          this.filteredProducts = (this.category) ?
+              this.products.filter( p => p.category === this.category) :
+              this.products;
+          });
+
+
+      }
 
   ngOnInit() {
   }
