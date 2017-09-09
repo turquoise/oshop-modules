a80a4aaaf8b4a37ab510400b01f128ca5b42a0a9
M Gosschalk <monica.gosschalk@btinternet.com>
2017-09-09 12:14:33 +0100

    added shipping form

--- src/app/check-out/check-out.component.ts
+++ src/app/check-out/check-out.component.ts
@@ -1,15 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ShoppingCartService } from '../shopping-cart.service';
+import { ShoppingCart } from '../models/shopping-cart';
+import { Subscription } from 'rxjs/Subscription';
+import { OrderService } from '../order.service';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-check-out',
   templateUrl: './check-out.component.html',
   styleUrls: ['./check-out.component.css']
 })
-export class CheckOutComponent implements OnInit {
+export class CheckOutComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  shipping = {};
+  cart: ShoppingCart;
+  user;
+  cartSubscription: Subscription;
+  userSubscription: Subscription;
 
-  ngOnInit() {
+  constructor(
+    private authService: AuthService,
+    private shoppingCartService: ShoppingCartService,
+    private orderService: OrderService ) { }
+
+  async ngOnInit() {
+    // I need a shopping cart and not an observable.
+    let cart$ = await this.shoppingCartService.getCart();
+    this.cartSubscription = cart$.subscribe( cart => this.cart = cart);
+    this.authService.user$.subscribe( user => this.user = user);
+  }
+
+  ngOnDestroy() {
+    this.cartSubscription.unsubscribe();
+    this.userSubscription.unsubscribe();
+  }
+
+  placeOrder() {
+    //console.log(this.shipping);
+    let order = {
+      userId: this.user.uid,
+      userEmail: this.user.email,
+      datePlaced: new Date().getTime(),
+      shipping: this.shipping,
+      items: this.cart.items.map( i => {
+        return {
+          product: {
+            title: i.title,
+            imageUrl: i.imageUrl,
+            price: i.price
+          },
+          quantity: i.quantity,
+          totalPrice: i.totalPrice
+        }
+      })
+    };
+    this.orderService.storeOrder(order);
   }
 
 }
